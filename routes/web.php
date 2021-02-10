261fb7311df043f8948adea3cc0cf3e64e069420
wbrak <webmasterbrak@gmail.com>
2021-02-10 18:11:12 +0100

    controller dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Backoffice\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/backoffice')->group(function(){
