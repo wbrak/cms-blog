9f0e5ad6cd9fd1ccc6ef517a6bb1bb088383a4b4
wbrak <webmasterbrak@gmail.com>
2021-02-10 03:54:18 +0100

    views masters structure

---
+++ resources/views/shop/master.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{!! str_replace('_', '-', app()->getLocale()) !!}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <meta name="csrf-token" content="{!! csrf_token() !!}">
+    <meta name="routeName" content="{!! Route::currentRouteName() !!}">
+    @section('meta')
+    @show
+
+    <title>{!! Config::get('company.name') !!} | @yield('title')</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="{!! url('storage/favicon.ico') !!}">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+
+    @livewireStyles
+
+    <script src="https://kit.fontawesome.com/447636fad2.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="d-flex" id="wrapper">
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+        @section('content')
+        @show
+
+    </div>
+    <!-- /#page-content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+@livewireScripts
+</body>
+</html>
